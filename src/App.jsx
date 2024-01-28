08069a2320e7dfea31506bfb80894fa2dd47620f
Ahmad Ali <ahmada02981@gmail.com>
2024-01-28 16:20:02 +0500

    Stage One

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio3</section>
+    <section>Portfolio3</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
